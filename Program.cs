5b2e5e8e941d7a54cbc894b6040ad9410ec7e2d2
Maksimov-Git <maksimov_alexey@inbox.ru>
2020-08-28 18:39:53 +0300

    Примеры для абстрактных классов и делегатов

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less_8_Interface
+{
+    interface IMyInterface
+    {
+        void Method();
+    }
+
+     class A : IMyInterface
+    {
+      public void  Method()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
